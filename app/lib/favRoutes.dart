6d021399485241ab2da462870f8146685dff0a6b
shlokdesai41103 <desaishlok41103@gmail.com>
2022-11-05 14:58:17 -0400

    favRoutes page view

---
+++ app/lib/favRoutes.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:app/main.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const favRoutes());
+}
+
+class favRoutes extends StatelessWidget {
+  const favRoutes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Favourite Routes',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Commutes'),
+      ),
+      body: const Center(
+        child: Text('This is where the users favorites will be displayed'),
+      ),
+    );
+  }
+}
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Login'),
     );
   }
 }
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You have pushed the buton this many time:',
             ),
             Text(
               '$_counter',
